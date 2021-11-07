c7120582f6187e8cbb7f24c931f7d0f16275d22e
İsmail Namdar <namdari@itu.edu.tr>
2021-11-07 14:53:16 +0300

    use dist folder and create esm build

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,21 @@
 import { babel } from '@rollup/plugin-babel';
+import pkg from './package.json'
 
 const config = {
   input: 'index.js',
-  output: {
-    dir: 'output',
-    format: 'iife'
-  },
+  output: [
+    {
+      file: pkg.main,
+      format: 'umd',
+      name: pkg.name,
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+      sourcemap: true
+    },
+  ],
   plugins: [babel({ babelHelpers: 'bundled' })]
 };
 
